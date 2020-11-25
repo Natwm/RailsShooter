01267458145085914b33f1a2b44b0d6e91884e7f
natwPine <natw.mezza@hotmail.fr>
2020-11-25 11:23:39 +0100

    [Script] EnnemyBehaviours

--- Assets/Scripts/BodyPartBehaviours.cs
+++ Assets/Scripts/BodyPartBehaviours.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class BodyPartBehaviours : MonoBehaviour
 {
     #region PARAM
+
     [Min(1)]
     [SerializeField] private int m_DamageMultiplicator;
     [SerializeField] private int m_DamageAdd;
     [SerializeField] private int m_AmountOfArmor;
     private HealthManager m_healthManager;
+
+
     #endregion
 
     private void Start()
