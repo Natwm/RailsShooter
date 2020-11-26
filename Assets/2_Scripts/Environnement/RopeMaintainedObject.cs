6fa88bdddd313e31a399d9dbbb96ff8460cad532
Pierre <pierre.rellic@gmail.com>
2020-11-26 17:40:46 +0100

    Add : RopeMaintainedObject first commit

---
+++ Assets/2_Scripts/Environnement/RopeMaintainedObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RopeMaintainedObject : HealthManager
+{
+    [SerializeField] private Transform ropeTransform;
+    private Rigidbody rig;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+        rig.isKinematic = true;
+    }
+
+    protected override void Death()
+    {
+        rig.isKinematic = false;
+        ropeTransform.gameObject.SetActive(false);
+    }
+}
