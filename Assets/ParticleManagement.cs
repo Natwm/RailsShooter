f333dfaa7e918e375cd47ab852d3844393c07e6c
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2020-11-27 22:14:39 +0100

    [Asset] Ajout main et thompson

---
+++ Assets/ParticleManagement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+
+public class ParticleManagement : MonoBehaviour
+{
+
+    public ParticleSystem shoot;
+
+    FMOD.Studio.EventInstance Bang;
+    [FMODUnity.EventRef] [SerializeField] private string BangSound;
+
+    FMOD.Studio.EventInstance Bang_Auto;
+    [FMODUnity.EventRef] [SerializeField] private string AutoSound;
+
+    FMOD.Studio.EventInstance Reload;
+    [FMODUnity.EventRef] [SerializeField] private string ReloadSound;
+
+    public ParticleSystem douille;
+
+    public ParticleSystem shoot_aotu;
+    public ParticleSystem douillasse;
+
+    public void PlayShoot()
+    {
+        shoot.Play();
+        Bang.start();
+        douille.Play();
+    }
+
+    public void PlayShootAuto()
+    {
+        shoot_aotu.Play();
+        Bang_Auto.start();
+        douillasse.Play();
+    }
+
+    public void PlayReload()
+    {
+        Reload.start();
+    }
+
+    void Start()
+    {
+        Bang = FMODUnity.RuntimeManager.CreateInstance(BangSound);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(Bang,transform, GetComponentInParent<Rigidbody>());
+
+        Bang_Auto = FMODUnity.RuntimeManager.CreateInstance(AutoSound);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(Bang_Auto, transform, GetComponentInParent<Rigidbody>());
+
+        Reload = FMODUnity.RuntimeManager.CreateInstance(ReloadSound);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(Reload, transform, GetComponentInParent<Rigidbody>());
+    }
+
+
+}
