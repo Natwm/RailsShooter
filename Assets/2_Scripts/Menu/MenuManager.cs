1b1f6f497d318d977552ce641d1fda09e6ff995e
Pierre <pierre.rellic@gmail.com>
2020-11-27 16:57:18 +0100

    Add : Menu Scene + Menu Script

---
+++ Assets/2_Scripts/Menu/MenuManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using DG.Tweening;
+
+public class MenuManager : MonoBehaviour {
+  public static MenuManager instance;
+  public GameObject startingMenuPage;
+  private MenuPage currentlyActiveMenuPage;
+  private MenuPage currentOverlay;
+
+  private Stack<MenuPage> overlays = new Stack<MenuPage>();
+
+  void Start() {
+    SetStaticInstance();
+    currentlyActiveMenuPage = startingMenuPage.GetComponent<MenuPage>();
+
+    foreach(MenuPage m in FindObjectsOfType<MenuPage>())
+    {
+      m.gameObject.SetActive(false);
+    }    
+    
+    currentlyActiveMenuPage.gameObject.SetActive(true);
+    Camera.main.transform.position = currentlyActiveMenuPage.cameraTransform.position;
+    Camera.main.transform.rotation = currentlyActiveMenuPage.cameraTransform.rotation;
+  }
+
+  private void SetStaticInstance () {
+    if (instance == null) {
+      instance = this;
+    }
+    else 
+      Debug.Log("There are multiple MenuManager instances");
+  }
+
+  public void SwapUIMenu( MenuPage to ) {
+    if (to.isOverlay) {
+      overlays.Push(to);
+      DisplayMenuPage(to);
+    }
+    else {
+      RemoveMenuPage(currentlyActiveMenuPage);
+      DisplayMenuPage(to);
+      currentlyActiveMenuPage = to;
+
+      Camera.main.transform.DOMove(to.cameraTransform.position, 2);
+      Camera.main.transform.DORotate(to.cameraTransform.rotation.eulerAngles, 2);
+    }
+  }
+
+  public void Return() {
+    if (overlays.Count != 0)
+      RemoveMenuPage(overlays.Pop());
+    else if (currentlyActiveMenuPage.returnMenu != null) {
+      SwapUIMenu(currentlyActiveMenuPage.returnMenu);
+    } 
+  }
+
+  private void RemoveMenuPage(MenuPage page) {
+    page.gameObject.SetActive(false);
+    if(!page.isOverlay) {
+      foreach(MenuPage overlay in overlays.ToArray())
+        RemoveMenuPage(overlay);
+      overlays.Clear();
+    }
+  }
+
+  private void DisplayMenuPage(MenuPage page) {
+    page.gameObject.SetActive(true);
+  }
+
+  public void Play() {
+    //GameManager.instance.LoadGame(gameSceneName); 
+  }
+
+  public void Leave() {
+    Application.Quit();
+  }
+}
---
+++ Assets/2_Scripts/Menu/MenuPage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MenuPage : MonoBehaviour
+{
+    public bool isOverlay = false;
+    public MenuPage returnMenu = null;
+    public Transform cameraTransform;
+}
