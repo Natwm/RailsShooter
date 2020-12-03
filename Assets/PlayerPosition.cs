154b7e3a38af13764436e739e0b0c90866ec631c
natwPine <natw.mezza@hotmail.fr>
2020-12-03 12:43:38 +0100

    [script]Condition script

---
+++ Assets/PlayerPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerPosition 
+{
+    public Transform position;
+
+    [Min (1)]
+    public int AmountOfEnnemiToKill = 1;
+
+    [Min(1)]
+    public float moveSpeed = 1f;
+}
